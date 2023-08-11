05864778e02312689a71a022cf83adbbd0b76eac
Ahmet GÜR <ahmetgur190758@gmail.com>
2023-08-11 16:47:39 +0300

    Week 4

---
+++ app/src/main/java/com/example/sisterslabproject/hafta4/odev2/Medium.kt
@@ -0,0 +1,6 @@
+package com.example.sisterslabproject.hafta4.odev2
+//Kotlin’de Dizilerle Veri Saklama ve İşlemler' Yazısı
+//https://medium.com/@ahmetgur190758/kotlinde-dizilerle-veri-saklama-ve-i%CC%87%C5%9Flemler-8dd9c4424c6
+
+//Kotlin’de Kalıtım ve Miras Bırakma
+//https://medium.com/@ahmetgur190758/kotlinde-kal%C4%B1t%C4%B1m-ve-miras-b%C4%B1rakma-c175e87657bb
