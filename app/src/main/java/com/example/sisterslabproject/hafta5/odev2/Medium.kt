3e58ec701b5f92d0adb42cc42ff5d2d451151283
Ahmet GÜR <ahmetgur190758@gmail.com>
2023-08-18 20:58:45 +0300

    Week 5

---
+++ app/src/main/java/com/example/sisterslabproject/hafta5/odev2/Medium.kt
@@ -0,0 +1,5 @@
+package com.example.sisterslabproject.hafta5.odev2
+
+//Listeler, RecyclerView ve Adapter Tasarımı  Konulu medium Linki
+
+//https://medium.com/@ahmetgur190758/android-uygulamalarda-veri-g%C3%B6steriminin-g%C3%BCc%C3%BC-listeler-recyclerview-ve-adapter-tasar%C4%B1m%C4%B1-31a7acfc2eaa
